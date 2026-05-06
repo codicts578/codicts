4e8b20a412f53db3319fa0d9a0e654bc016c0e1b
aishasiddiqah <codicts578@gmail.com>
2026-05-06 10:35:39 +0400

    Update data.js

--- frontend/Pages/Favourites-Page/data.js
+++ frontend/Pages/Favourites-Page/data.js
@@ -1,15 +1,15 @@
 // data.js
 const UI_COMPONENTS = [
-    { name: "Button Component 1", category: "buttons", tag: "FREE", src: "../../../frontend/images/Components Page/button1.mp4", mediaType: "video", link: "/frontend/Pages/Design and Code Editor/Buttons/Button 1/buttonComponent1.html" },
-    { name: "Button Component 2", category: "buttons", tag: "FREE", src: "../../../frontend/images/Components Page/button2.mp4", mediaType: "video", link: "/frontend/Pages/Design and Code Editor/Buttons/Button 2/buttonComponent2.html" },
-    { name: "Button Component 3", category: "buttons", tag: "PRO", src: "../../../frontend/images/Components Page/button3.mp4", mediaType: "video", link: "/frontend/Pages/Design and Code Editor/Buttons/Button 3/buttonComponent3.html" },
-    { name: "Button Component 4", category: "buttons", tag: "PRO", src: "../../../frontend/images/Components Page/button4.mp4", mediaType: "video", link: "/frontend/Pages/Design and Code Editor/Buttons/Button 4/buttonComponent4.html" },
+    { name: "Button Component 1", category: "buttons", tag: "FREE", src: "../../../frontend/images/Components-Page/button1.mp4", mediaType: "video", link: "/frontend/Pages/Code-Editor/Buttons/Button-1/buttonComponent1.html" },
+    { name: "Button Component 2", category: "buttons", tag: "FREE", src: "../../../frontend/images/Components-Page/button2.mp4", mediaType: "video", link: "/frontend/Pages/Code-Editor/Buttons/Button-2/buttonComponent2.html" },
+    { name: "Button Component 3", category: "buttons", tag: "PRO", src: "../../../frontend/images/Components-Page/button3.mp4", mediaType: "video", link: "/frontend/Pages/Code-Editor/Buttons/Button-3/buttonComponent3.html" },
+    { name: "Button Component 4", category: "buttons", tag: "PRO", src: "../../../frontend/images/Components-Page/button4.mp4", mediaType: "video", link: "/frontend/Pages/Code-Editor/Buttons/Button-4/buttonComponent4.html" },
 
-    { name: "Card Component 1", category: "cards", tag: "FREE", src: "../../../frontend/images/Components Page/card1.mp4", mediaType: "video", link: "/frontend/Pages/Design and Code Editor/Cards/Card 1/cardComponent1.html" },
-    { name: "Card Component 2", category: "cards", tag: "PRO", src: "../../../frontend/images/Components Page/card2.mp4", mediaType: "video", link: "/frontend/Pages/Design and Code Editor/Cards/Card 2/cardComponent2.html" },
-    { name: "Card Component 3", category: "cards", tag: "PRO", src: "../../../frontend/images/Components Page/card3.mp4", mediaType: "video", link: "/frontend/Pages/Design and Code Editor/Cards/Card 3/cardComponent3.html" },
+    { name: "Card Component 1", category: "cards", tag: "FREE", src: "../../../frontend/images/Components-Page/card1.mp4", mediaType: "video", link: "/frontend/Pages/Code-Editor/Cards/Card-1/cardComponent1.html" },
+    { name: "Card Component 2", category: "cards", tag: "PRO", src: "../../../frontend/images/Components-Page/card2.mp4", mediaType: "video", link: "/frontend/Pages/Code-Editor/Cards/Card-2/cardComponent2.html" },
+    { name: "Card Component 3", category: "cards", tag: "PRO", src: "../../../frontend/images/Components-Page/card3.mp4", mediaType: "video", link: "/frontend/Pages/Code-Editor/Cards/Card-3/cardComponent3.html" },
 
-    { name: "Form Component 1", category: "forms", tag: "FREE", src: "../../../frontend/images/Components Page/Form1.png", mediaType: "image", link: "/frontend/Pages/Design and Code Editor/Forms/Login Forms/loginComponent1.html" }
+    { name: "Form Component 1", category: "forms", tag: "FREE", src: "../../../frontend/images/Components-Page/Form1.png", mediaType: "image", link: "/frontend/Pages/Code-Editor/Forms/Login-Forms/loginComponent1.html" }
 ];
 
 function generateCardHTML(item, isLiked, index) { // Added index here
